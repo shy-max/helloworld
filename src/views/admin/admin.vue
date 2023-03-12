73c968d98514e7ab5ac8a1714c47855daed3dd46
shy-max <2633075052@qq.com>
2023-03-12 20:35:50 +0800

    添加admin组件

---
+++ src/views/admin/admin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        <!-- teacher student -->
+        <router-view></router-view> 
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'admin',
+        
+    }
+</script>
